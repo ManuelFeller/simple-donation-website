bd8a903eb7f6e27edfe5b65c67e13a20dfa40770
Christian Schweikert <christian.schweikert@boehringer-ingelheim.com>
2022-04-29 15:57:28 +0200

    initiave page with cards

--- donation-tracker/src/pages/index.tsx
+++ donation-tracker/src/pages/index.tsx
@@ -1,53 +1,54 @@
-import * as React from "react"
-import TestComponent from "../components/testComponent"
+import * as React from "react";
+import TestComponent from "../components/testComponent";
+import Initiative1Page, { Task } from "./initiatives/initiative1";
 
 // styles
 const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 const headingAccentStyles = {
   color: "#663399",
-}
+};
 const paragraphStyles = {
   marginBottom: 48,
-}
+};
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
   borderRadius: 4,
-}
+};
 const listStyles = {
   marginBottom: 96,
   paddingLeft: 0,
-}
+};
 const listItemStyles = {
   fontWeight: 300,
   fontSize: 24,
   maxWidth: 560,
   marginBottom: 30,
-}
+};
 
 const linkStyle = {
   color: "#8954A8",
   fontWeight: "bold",
   fontSize: 16,
   verticalAlign: "5%",
-}
+};
 
 const docLinkStyle = {
   ...linkStyle,
   listStyleType: "none",
   marginBottom: 24,
-}
+};
 
 const descriptionStyle = {
   color: "#232129",
@@ -55,13 +56,13 @@ const descriptionStyle = {
   marginTop: 10,
   marginBottom: 0,
   lineHeight: 1.25,
-}
+};
 
 const docLink = {
   text: "TypeScript Documentation",
   url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
   color: "#8954A8",
-}
+};
 
 const badgeStyle = {
   color: "#fff",
@@ -77,7 +78,7 @@ const badgeStyle = {
   top: -2,
   marginLeft: 10,
   lineHeight: 1,
-}
+};
 
 // data
 const links = [
@@ -124,12 +125,19 @@ const links = [
       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
     color: "#663399",
   },
-]
+];
+
+const tasks: Task[] = [
+  { name: "Multitool / Muli-Tool", needed: 12, donated: 3, required: 15 },
+  { name: "Sleeping bag / Schlafsack", needed: 6, donated: 9, required: 15 }
+];
 
 // markup
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
+      <Initiative1Page tasks={tasks}></Initiative1Page>
+      {/*
       <TestComponent></TestComponent>
       <title>Home Page</title>
       <h1 style={headingStyles}>
@@ -174,8 +182,9 @@ const IndexPage = () => {
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
+       */}
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
