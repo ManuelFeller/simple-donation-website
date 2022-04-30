71b26d873c441d8a8673034c56fd04794c162dc5
Manuel Feller <github@devsvr.ws>
2022-04-30 19:37:06 +0200

    Adding a Material UI Navigation Bar

--- donation-tracker/src/components/layout.tsx
+++ donation-tracker/src/components/layout.tsx
@@ -1,12 +1,112 @@
-import { Link } from 'gatsby';
+import { Link, navigate } from 'gatsby';
+import '../styles.scss';
 import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const mainLevelPages = [
+  { name: 'Home / Start', link: '/', key: 'home' },
+  { name: 'Imprint / Impressum', link: '/imprint/', key: 'imprint'}
+];
 
 const LayoutModule = (props: any) => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  }
+  const handleClickOnNavMenu = (event: React.MouseEvent<HTMLElement>, itemLink: string) => {
+    // this handles the navigation if JavaScript is active
+    event.preventDefault();
+    navigate(itemLink)
+    setAnchorElNav(null);
+  };
   return(
     <>
-      <div>
-        Header
-      </div>
+      <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            Help Ukraine Desktop
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {mainLevelPages.map((page) => (
+                <MenuItem href={page.link} key={page.key} onClick={(event) => handleClickOnNavMenu(event, page.link)}>
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+          >
+            Help Ukraine Mobile
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {mainLevelPages.map((page) => (
+              <Button
+                href={page.link}
+                key={page.key}
+                onClick={(event) => handleClickOnNavMenu(event, page.link)}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+
+          
+        </Toolbar>
+      </Container>
+    </AppBar>
       <div>
         {props.children}
       </div>
