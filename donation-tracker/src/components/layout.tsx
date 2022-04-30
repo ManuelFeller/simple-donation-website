6cf584f061521540d3b14ece4bccafe20c69a2cc
Manuel Feller <github@devsvr.ws>
2022-04-30 20:23:27 +0200

    some tweaking

--- donation-tracker/src/components/layout.tsx
+++ donation-tracker/src/components/layout.tsx
@@ -15,9 +15,10 @@ import MenuItem from '@mui/material/MenuItem';
 
 /* ToDo: import this from a central location */
 const mainLevelPages = [
-  { name: 'Home / Start', link: '/' },
+  { name: 'Start', link: '/' },
   { name: 'Imprint / Impressum', link: '/imprint/' }
 ];
+const titleText = 'Help Ukraine';
 
 const LayoutModule = (props: any) => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
@@ -36,84 +37,76 @@ const LayoutModule = (props: any) => {
   return(
     <>
       <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-          >
-            Help Ukraine Desktop
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {mainLevelPages.map((page, index) => (
-                <MenuItem href={page.link} key={'mMenuItem'.concat(index.toString())} onClick={(event) => handleClickOnNavMenu(event, page.link)}>
-                  <Typography textAlign="center">{page.name}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
-            Help Ukraine Mobile
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {mainLevelPages.map((page, index) => (
-              <Button
-                href={page.link}
-                key={'dMenuItem'.concat(index.toString())}
-                onClick={(event) => handleClickOnNavMenu(event, page.link)}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+              {titleText}
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
               >
-                {page.name}
-              </Button>
-            ))}
-          </Box>
-
-          
-        </Toolbar>
-      </Container>
-    </AppBar>
-      <div>
-        {props.children}
-      </div>
-      <div>
-        <Link to='/'>Home / Start</Link> - <Link to='/imprint/'>Imprint / Impressum</Link>
-      </div>
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {mainLevelPages.map((page, index) => (
+                  <MenuItem href={page.link} key={'mMenuItem'.concat(index.toString())} onClick={(event) => handleClickOnNavMenu(event, page.link)}>
+                    <Typography textAlign="center">{page.name}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              {titleText}
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {mainLevelPages.map((page, index) => (
+                <Button
+                  href={page.link}
+                  key={'dMenuItem'.concat(index.toString())}
+                  onClick={(event) => handleClickOnNavMenu(event, page.link)}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page.name}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      {props.children}
     </>
   );
 }
