8547230376346760a050b1401f024f082ec52198
Manuel Feller <github@devsvr.ws>
2022-05-01 19:45:34 +0200

    adding some dummy content to the start page

--- donation-tracker/src/pages/index.tsx
+++ donation-tracker/src/pages/index.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
 import LayoutModule from '../components/layout';
 
+import { Button, Container, Typography } from '@mui/material';
+
 const IndexPage = () => {
-  return <LayoutModule></LayoutModule>;
+  return <LayoutModule>
+    <Container maxWidth="lg">
+      &nbsp;
+      <Typography variant="h2" component="h1">
+        Help Ukraine
+      </Typography>
+      <Typography component="div">
+        Some more details about this page, how things work, ...
+      </Typography>
+    </Container>
+  </LayoutModule>;
 };
 
 export default IndexPage;
