5a74f291b6becc481578fb95bf41c1a81488ee0f
Manuel Feller <github@devsvr.ws>
2022-05-02 16:10:16 +0200

    removing layout module from all initative pages

--- donation-tracker/src/pages/initiatives/civilian-help-1.tsx
+++ donation-tracker/src/pages/initiatives/civilian-help-1.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import LayoutModule from '../../components/layout';
 import CampaignDetailsModule from '../../modules/campaignDetails';
 
 const CampaignDetailsPage = (props: any) => {
