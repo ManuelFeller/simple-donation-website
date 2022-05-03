d54597f90711af807fc57baadc78b97d4cf9834d
Manuel Feller <github@devsvr.ws>
2022-05-03 17:40:35 +0200

    made start page content expandable

--- donation-tracker/src/pages/index.tsx
+++ donation-tracker/src/pages/index.tsx
@@ -6,11 +6,22 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import { navigate } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
+
+  const [expanded, setExpanded] = React.useState<string | false>(false);
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
 
   const handleClickOnLink = (event: React.MouseEvent<HTMLElement>, itemLink: string) => {
     // this handles the navigation if JavaScript is active
@@ -35,33 +46,56 @@ const IndexPage = () => {
             transformOptions={{trim: 20}}
           />
           &nbsp;
-          <Typography component="div">
-            🇬🇧 We are a group of employees at a company in Ingelheim that have friends and family from or in the Ukraine.
-            Because of that we know very well what is needed where right now, and that is why we are organizing donation campaigns.<br/>
-            The main goal of this website is to allow all our colleagues to do coordinated donations while being updated about the progress
-            - without the need of access to the company intranet. This also allows friends and families of them to participate if they want.<br/>
-            Details what is needed and how you can send it to us (so that we can forward it to the people in need) are available in the details of the individual campaigns.
-          </Typography>
-          &nbsp;
-          <Typography component="div">
-            Please understand that we are only publishing limited information about our identities.
-            Even if we hope that this will feel like a bad dream tomorrow we want to make sure that - in case of an escalation - we don't get in trouble for helping.
-            Or that we get threatened or similar things because some people disagree with this activity.
-          </Typography>
-          &nbsp;
-          <Typography component="div">
-            🇩🇪 Wir sind eine Gruppe von Mitarbeitern eines Unternehmen in Ingelheim die Freunde und Familie in oder aus der Ukraine haben.
-            Aus diesem Grund wissen wir recht genau was gerade wo benötigt wird, und das ist der Grund warum wir Spenden-Kampagnen durchführen.<br/>
-            Das Haupt-Ziel dieser Webseite ist unseren Kollegen zu ermöglichen koordiniert zu spenden und über den Fortschritt informiert zu bleiben
-            - ohne auf das Firmen-Intranet zugreifen zu müssen. Das ermöglicht dann auch deren Familien und Freunden teilzunehmen (wenn diese das möchten).<br/>
-            Details was benötigt wird und wie man uns Spenden zukommen lassen kann (so das wir das wiederum weiterleiten können) ist in den Details der einzelnen Kampagnen aufgeführt.
-          </Typography>
-          &nbsp;
-          <Typography component="div">
-            Bitte haben Sie Verständnis das wir hier nur begrenzt Informationen über unsere Identität veröffentlichen.
-            Auch wenn wir hoffen das morgen alles wie ein böser Traum wirkt möchten wir dennoch verhindern das wir bei einer Eskalation
-            durch unsere Hilfe Probleme bekommen. Oder durch Gegner solcher Tätigkeiten gefährdet oder ähnliches werden.
-          </Typography>
+          <Accordion elevation={4} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: '40px', flexShrink: 0 }} variant="h5" component="div">
+                🇬🇧
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }}>
+                {expanded !== 'panel1' && <>
+                  We are a group of employees at a company in Ingelheim that have friends and family from or in the Ukraine <i>... click / tap to read more</i>
+                </>}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                We are a group of employees at a company in Ingelheim that have friends and family from or in the Ukraine.
+                Because of that we know very well what is needed where right now, and that is why we are organizing donation campaigns.<br/>
+                The main goal of this website is to allow all our colleagues to do coordinated donations while being updated about the progress
+                - without the need of access to the company intranet. This also allows friends and families of them to participate if they want.
+                Details what is needed and how you can send it to us (so that we can forward it to the people in need) are available in the details of the individual campaigns.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion elevation={4} expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2bh-content"
+              id="panel2bh-header"
+            >
+              <Typography sx={{ width: '40px', flexShrink: 0 }} variant="h5" component="div">
+                🇩🇪
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }}>
+                {expanded !== 'panel2' && <>
+                  Wir sind eine Gruppe von Mitarbeitern eines Unternehmen in Ingelheim die Freunde und Familie in oder aus der Ukraine haben <i>... anklicken / antippen für mehr</i>
+                </>}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Wir sind eine Gruppe von Mitarbeitern eines Unternehmen in Ingelheim die Freunde und Familie in oder aus der Ukraine haben.
+                Aus diesem Grund wissen wir recht genau was gerade wo benötigt wird, und das ist der Grund warum wir Spenden-Kampagnen durchführen.<br/>
+                Das Haupt-Ziel dieser Webseite ist unseren Kollegen zu ermöglichen koordiniert zu spenden und über den Fortschritt informiert zu bleiben
+                - ohne auf das Firmen-Intranet zugreifen zu müssen. Das ermöglicht dann auch deren Familien und Freunden teilzunehmen (wenn diese das möchten).<br/>
+                Details was benötigt wird und wie man uns Spenden zukommen lassen kann (so das wir das wiederum weiterleiten können) ist in den Details der einzelnen Kampagnen aufgeführt.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </CardContent>
         <CardActions>
           <Button
