3499adddec1f8248a11ddf032e2f23a6a1209178
Manuel Feller <github@devsvr.ws>
2022-05-06 11:56:48 +0200

    setting up translation sources

--- donation-tracker/gatsby-config.ts
+++ donation-tracker/gatsby-config.ts
@@ -28,6 +28,32 @@ const config: GatsbyConfig = {
       "path": "./src/pages/"
     },
     __key: "pages"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "locale",
+      "path": "./src/locales/"
+    },
+    __key: "locale"
+  },
+  {
+    resolve: `gatsby-plugin-react-i18next`,
+    options: {
+      localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+      languages: [`en`, `de`],
+      defaultLanguage: `de`,
+      // if you are using Helmet, you must include the siteUrl, and make sure you add http:https
+      siteUrl: PageConfiguration.PageUrl,
+      // you can pass any i18next options
+      i18nextOptions: {
+        interpolation: {
+          escapeValue: false // not needed for react as it escapes by default
+        },
+        keySeparator: false,
+        nsSeparator: false
+      },
+      pages: []
+    }
   }]
 };
 
