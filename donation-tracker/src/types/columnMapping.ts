25c0548a98c2f12eab0f8095fdf952c20c363003
Manuel Feller <github@devsvr.ws>
2022-05-11 18:02:37 +0200

    atting a type for column mapping

---
+++ donation-tracker/src/types/columnMapping.ts
@@ -0,0 +1,10 @@
+export interface ColumnMapping {
+	languages: string[],
+	item: number[],
+	unit: number[],
+	form: number[],
+	campaignId: number,
+	need: number,
+	donated: number,
+	lastChange: number
+}
