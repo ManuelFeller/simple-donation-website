9166ac6f26d0b56bd8ae5a1c1026f0e13c40aa9c
Manuel Feller <github@devsvr.ws>
2022-05-11 21:07:22 +0200

    adding multi language form links

--- donation-tracker/src/components/donationPill.tsx
+++ donation-tracker/src/components/donationPill.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
-
+import { useTranslation, I18nextContext } from 'gatsby-plugin-react-i18next';
 import { Button } from '@mui/material';
 
 import { DonationItem } from '../types/donationItem';
 import { Campaign } from '../types/campaign';
-import { useTranslation } from 'react-i18next';
 
 const DonationPill = ({ campaign, donationItem }: { donationItem: DonationItem; campaign: Campaign }) => {
   const { t } = useTranslation();
+  const langContext = React.useContext(I18nextContext);
   return donationItem.remainingNeed ? (
     <Button
-      href={campaign.RegistrationFormUrl!}
+      href={donationItem.formLink[langContext.language]!}
       target="_blank"
       variant="contained"
       sx={{
@@ -24,7 +24,7 @@ const DonationPill = ({ campaign, donationItem }: { donationItem: DonationItem; 
     </Button>
   ) : (
     <Button
-      href={campaign.RegistrationFormUrl!}
+      href={donationItem.formLink[langContext.language]!}
       target="_blank"
       variant="outlined"
       color="success"
